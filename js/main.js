57ee0a070afd72ec169ad888b5912a4e7b928fdc
ANVK1729 <vijaykrishna@student.tce.edu>
2025-02-10 00:08:23 +0530

    Checkout Page Minor Changes

--- js/main.js
+++ js/main.js
@@ -64,3 +64,16 @@
     
 })(jQuery);
 
+function adjustPadding() {
+    var header = document.querySelector('.header');
+    var headerHeight = header.offsetHeight;
+    document.body.style.paddingTop = headerHeight + 'px';
+}
+
+document.addEventListener("DOMContentLoaded", adjustPadding);
+window.addEventListener("resize", adjustPadding);
+
+function removeCartItem(button) {
+    var cartItem = button.closest('.cart-item');
+    cartItem.remove();
+}
